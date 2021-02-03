a656f438e9a3e576709941aa11850a2ac63418e5
Jonathan Ventura <jventu09@calpoly.edu>
2021-02-02 21:08:24 -0800

    adding python renderer

---
+++ render.py
@@ -0,0 +1,93 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+import time
+
+#import pygame as pg
+#from pygame.locals import *
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+from rendertools import *
+
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    parser.add_argument('--mode',default='cylindrical')
+    args = parser.parse_args()
+    
+    width = 640
+    height = 480
+    fovy = 90
+
+    glutInit()
+    glutInitDisplayMode(GLUT_RGBA|GLUT_3_2_CORE_PROFILE)
+    glutInitWindowSize(width,height)
+    glutInitWindowPosition(0,0)
+    window = glutCreateWindow('window')
+    #glutHideWindow()
+    #pg.init()
+    #pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+    #pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 2)
+    #pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK,
+                                #pg.GL_CONTEXT_PROFILE_CORE)
+    #pg.display.set_mode((width,height), DOUBLEBUF|OPENGL)
+
+    
+    min_depth = 1.
+    max_depth = 100.
+    depths = 1./np.linspace(1./max_depth,1./min_depth,32,endpoint=True)
+     
+    if args.mode == 'cylindrical':
+        meshes = [Cylinder(bottom=-1*depth,top=1*depth,radius=depth,texturepath=os.path.join(args.path,'courtyard_%d.png'%i)) for i,depth in enumerate(depths)]
+    else:
+        meshes = [Plane(depth=depth,texturepath=os.path.join(args.path,'image1_%d.png'%i)) for i,depth in enumerate(depths)]
+    
+    renderer = Renderer(meshes,width=width,height=height)
+
+    time0 = time.time()
+    def do_render():
+        #print(time.time()-time0)
+        t = (time.time() - time0)/10
+        eye = np.array([np.sin(t*10)/10,0,0])
+        #eye = np.array([0,0,0])
+        up = np.array([0,1,0])
+        #target = eye+np.array([0,0,-1])
+        target = eye+np.array([np.sin(t),0,-np.cos(t)])
+
+        view_matrix = lookAt(eye,target,up)
+        proj_matrix = perspective(fovy, width/height, 0.1, 1000.0)
+        mvp_matrix = proj_matrix@view_matrix
+
+        renderer.render(mvp_matrix)
+
+        glutSwapBuffers()
+    
+    def keyboard(key,x,y):
+        print(key)
+        ch = key.decode("utf-8")
+        print(ch)
+        if key == GLUT_KEY_LEFT:
+            eye[0] -= .1
+        elif key == GLUT_KEY_RIGHT:
+            eye[0] += .1
+        print(eye)
+
+    glutDisplayFunc(do_render)
+    glutIdleFunc(do_render)
+    glutKeyboardFunc(keyboard)
+    glutMainLoop()
+
+    #while True:
+        #for event in pg.event.get():
+            #if event.type == pg.QUIT:
+                #pg.quit()
+                #quit()
+    #
+    #renderer.render(mvp_matrix)
+    #
+    #pg.display.flip()
+    #pg.time.wait(10)
---
+++ rendertools.py
@@ -0,0 +1,300 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+import numpy as np
+from imageio import imread, imwrite
+from scipy.special import cotdg
+
+vert = """
+#version 330
+
+layout(location = 0) in vec3 iPosition;
+layout(location = 1) in vec2 iTexCoord;
+
+out vec2 texCoord;
+
+uniform mat4 mvp;
+
+void main()
+{
+    gl_Position = mvp * vec4(iPosition,1);
+    texCoord = iTexCoord;
+}
+"""
+
+frag = """
+#version 330
+
+uniform sampler2D sampler;
+
+in vec2 texCoord;
+
+out vec4 color;
+
+void main()
+{
+    color = texture(sampler,texCoord);
+}
+"""
+
+class Mesh:
+    def __init__(self):
+        self.vertices = []
+        self.texCoords = []
+        self.faces = []
+
+    def initializeVertexBuffer(self):
+        """
+        Assign the triangular mesh data and the triplets of vertex indices that form the triangles (index data) to VBOs
+        """
+        self.vertexBufferObject = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vertexBufferObject)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices, GL_STATIC_DRAW)
+        
+        self.texCoordBufferObject = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.texCoordBufferObject)
+        glBufferData(GL_ARRAY_BUFFER, self.texCoords, GL_STATIC_DRAW)
+        
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+        
+        self.indexBufferObject = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.indexBufferObject)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, self.indices, GL_STATIC_DRAW)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)
+
+    def initializeTexture(self):
+        self.textureID = glGenTextures(1)
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+        glBindTexture(GL_TEXTURE_2D, self.textureID)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, self.texture.shape[1], self.texture.shape[0], 0, GL_RGBA, GL_UNSIGNED_BYTE, self.texture)
+
+    def initializeVertexArray(self):
+        """
+        Creates the VAO to store the VBOs for the mesh data and the index data, 
+        """
+        self.vertexArrayObject = glGenVertexArrays(1)
+        glBindVertexArray(self.vertexArrayObject)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.vertexBufferObject)
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.texCoordBufferObject)
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, None)
+        
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.indexBufferObject)
+        
+        glBindVertexArray(0)
+
+    def render(self):
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self.textureID)
+        glBindVertexArray(self.vertexArrayObject)
+        glDrawElements(GL_TRIANGLES, self.indices.size, GL_UNSIGNED_SHORT, None)
+
+class Cylinder(Mesh):
+    def __init__(self,bottom,top,radius,texturepath,nsegments=1024):
+        thetarange = np.linspace(0,2*np.pi,nsegments)
+        self.vertices = []
+        self.texCoords = []
+        for theta in thetarange:
+            x = radius*np.cos(theta)
+            z = radius*np.sin(theta)
+            self.vertices.append(np.array([x,bottom,z]))
+            self.vertices.append(np.array([x,top,z]))
+            self.texCoords.append(np.array([theta/(2*np.pi),0]))
+            self.texCoords.append(np.array([theta/(2*np.pi),1]))
+        self.indices = []
+        for i in range(len(self.vertices)):
+            self.indices.append(np.array([i,(i+1)%len(self.vertices),(i+2)%len(self.vertices)]))
+        self.vertices = np.stack(self.vertices,axis=0).astype(np.float32)
+        self.texCoords = np.stack(self.texCoords,axis=0).astype(np.float32)
+        self.indices = np.stack(self.indices,axis=0).astype(np.uint16)
+
+        self.texture = imread(texturepath)
+        self.texture = np.flipud(self.texture)
+
+class Plane(Mesh):
+    def __init__(self,depth,texturepath):
+        self.vertices = [[-1,-1, -1],
+                         [ 1,-1, -1],
+                         [ 1, 1, -1],
+                         [-1, 1, -1]]
+        self.texCoords = [[0,0],
+                          [1,0],
+                          [1,1],
+                          [0,1]]
+        self.indices = [[0,1,2],[2,3,0]]
+        self.vertices = np.array(self.vertices).astype(np.float32)
+        self.vertices *= depth
+        self.texCoords = np.array(self.texCoords).astype(np.float32)
+        self.indices = np.array(self.indices).astype(np.uint16)
+
+        self.texture = imread(texturepath)
+        self.texture = np.flipud(self.texture)
+
+class Renderer:
+    def __init__(self,meshes,width,height):
+        self.meshes = meshes
+        self.width = width
+        self.height = height
+
+        shaderDict = {GL_VERTEX_SHADER: vert, GL_FRAGMENT_SHADER: frag}
+        
+        self.initializeShaders(shaderDict)
+        
+        # Set the dimensions of the viewport
+        glViewport(0, 0, width, height)
+        
+        # Performs z-buffer testing
+        glEnable(GL_DEPTH_TEST)
+        glDepthMask(GL_TRUE)
+        glDepthFunc(GL_LEQUAL)
+        glDepthRange(0.0, 1.0)
+        
+        # With all of our data defined, we can initialize our VBOs, FBO, and VAO to the OpenGL context to prepare for rendering
+        #self.initializeFramebufferObject()
+
+        for mesh in self.meshes:
+            mesh.initializeVertexBuffer()
+            mesh.initializeVertexArray()
+            mesh.initializeTexture()
+
+    def initializeShaders(self,shaderDict):
+        """
+        Compiles each shader defined in shaderDict, attaches them to a program object, and links them (i.e., creates executables that will be run on the vertex, geometry, and fragment processors on the GPU). This is more-or-less boilerplate.
+        """
+        shaderObjects = []
+        self.shaderProgram = glCreateProgram()
+        
+        for shaderType, shaderString in shaderDict.items():
+            shaderObjects.append(glCreateShader(shaderType))
+            glShaderSource(shaderObjects[-1], shaderString)
+            
+            glCompileShader(shaderObjects[-1])
+            status = glGetShaderiv(shaderObjects[-1], GL_COMPILE_STATUS)
+            if status == GL_FALSE:
+                if shaderType is GL_VERTEX_SHADER:
+                    strShaderType = "vertex"
+                elif shaderType is GL_GEOMETRY_SHADER:
+                    strShaderType = "geometry"
+                elif shaderType is GL_FRAGMENT_SHADER:
+                    strShaderType = "fragment"
+                raise RuntimeError("Compilation failure (" + strShaderType + " shader):\n" + glGetShaderInfoLog(shaderObjects[-1]).decode('utf-8'))
+            
+            glAttachShader(self.shaderProgram, shaderObjects[-1])
+        
+        glLinkProgram(self.shaderProgram)
+        status = glGetProgramiv(self.shaderProgram, GL_LINK_STATUS)
+        
+        if status == GL_FALSE:
+            raise RuntimeError("Link failure:\n" + glGetProgramInfoLog(self.shaderProgram).decode('utf-8'))
+            
+        for shader in shaderObjects:
+            glDetachShader(self.shaderProgram, shader)
+            glDeleteShader(shader)
+
+    def configureShaders(self,mvp):
+        mvpUnif = glGetUniformLocation(self.shaderProgram, "mvp")
+
+        glUseProgram(self.shaderProgram)
+        glUniformMatrix4fv(mvpUnif, 1, GL_TRUE, mvp)
+
+        samplerUnif = glGetUniformLocation(self.shaderProgram, "sampler")
+        glUniform1i(samplerUnif, 0)
+
+        glUseProgram(0)
+
+    def initializeFramebufferObject(self):
+        """
+        Create an FBO and assign a texture buffer to it for the purpose of offscreen rendering to the texture buffer
+        """
+        self.renderedTexture = glGenTextures(1)
+        
+        glBindTexture(GL_TEXTURE_2D, self.renderedTexture)
+        
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, self.width, self.height, 0, GL_RGB, GL_FLOAT, None)
+        
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        
+        glBindTexture(GL_TEXTURE_2D, 0)
+        
+        self.depthRenderbuffer = glGenRenderbuffers(1)
+        
+        glBindRenderbuffer(GL_RENDERBUFFER, self.depthRenderbuffer)
+        
+        glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, self.width, self.height)
+        
+        glBindRenderbuffer(GL_RENDERBUFFER, 0)
+        
+        self.framebufferObject = glGenFramebuffers(1)
+        
+        glBindFramebuffer(GL_FRAMEBUFFER, self.framebufferObject)
+        
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, self.renderedTexture, 0)
+        
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, self.depthRenderbuffer)
+        
+        if glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE:
+            raise RuntimeError('Framebuffer binding failed, probably because your GPU does not support this FBO configuration.')
+        
+        glBindFramebuffer(GL_FRAMEBUFFER, 0)
+    
+    def render(self,mvp):
+        self.configureShaders(mvp)
+
+        glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+        glUseProgram(self.shaderProgram)
+
+        #glBindFramebuffer(GL_FRAMEBUFFER, self.framebufferObject)
+
+        glClearColor(0.0, 0.0, 0.0, 0.0)
+        glClearDepth(1.0)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        for mesh in self.meshes:
+            mesh.render()
+
+        glUseProgram(0)
+
+        #glPixelStorei(GL_PACK_ALIGNMENT, 1)
+        #glReadBuffer(GL_COLOR_ATTACHMENT0)
+        #data = glReadPixels(0, 0, self.width, self.height, GL_RGB, GL_FLOAT)
+        #rendering = np.frombuffer(data, dtype = np.float32).reshape(self.height, self.width, 3)
+
+        #return np.flipud(rendering)
+
+def normalize(vec):
+    return vec / np.linalg.norm(vec)
+
+def lookAt(eye,center,up):
+    F = center - eye
+    f = normalize(F)
+    s = np.cross(f,normalize(up))
+    u = np.cross(normalize(s),f)
+
+    M = np.eye(4)
+    M[0,0:3] = s
+    M[1,0:3] = u
+    M[2,0:3] = -f
+    
+    T = np.eye(4)
+    T[0:3,3] = -eye
+
+    return M@T
+
+def perspective(fovy,aspect,zNear,zFar):
+    f = cotdg(fovy/2)
+    M = np.zeros((4,4))
+    M[0,0] = f/aspect
+    M[1,1] = f
+    M[2,2] = (zFar+zNear)/(zNear-zFar)
+    M[2,3] = (2*zFar*zNear)/(zNear-zFar)
+    M[3,2] = -1
+    return M
+

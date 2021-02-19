55be1df0625b5fa918d54862c5d63e26a955be35
RichaGadgil <gadgilricha@gmail.com>
2021-02-19 09:40:46 -0800

    unity implementation

---
+++ MyFirstGame/Assets/CamRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ 
+public class CamRotation: MonoBehaviour {
+ 
+    public float rotationSpeed = 1.0f;
+    
+
+    void FixedUpdate() {
+        Vector3 rotation = transform.eulerAngles;
+ 
+        rotation.x += Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime; // Standart Left-/Right Arrows and A & D Keys
+ 
+        transform.eulerAngles = rotation;
+    }
+}

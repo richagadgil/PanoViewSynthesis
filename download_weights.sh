d97826802fc3849c79c6744f0699cfe7bd1470bc
Jonathan Ventura <jventu09@calpoly.edu>
2021-03-14 20:21:14 -0700

    adding single_view_mpi library to repo

--- download_weights.sh
+++ download_weights.sh
@@ -1,6 +1,6 @@
-echo Fetching code from github...
-apt install subversion
-svn export --force https://github.com/google-research/google-research/trunk/single_view_mpi
+#echo Fetching code from github...
+#apt install subversion
+#svn export --force https://github.com/google-research/google-research/trunk/single_view_mpi
 
 echo
 echo Fetching trained model weights...
@@ -8,3 +8,4 @@ rm single_view_mpi_full_keras.tar.gz
 rm -rf single_view_mpi_full
 wget https://storage.googleapis.com/stereo-magnification-public-files/models/single_view_mpi_full_keras.tar.gz
 tar -xzvf single_view_mpi_full_keras.tar.gz
+rm single_view_mpi_full_keras.tar.gz

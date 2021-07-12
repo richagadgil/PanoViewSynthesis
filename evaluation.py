92d838cfcb311cec573f738ee913a18c5a84b0e5
RichaGadgil <gadgilricha@gmail.com>
2021-07-11 22:04:12 -0700

    calculate sigma

--- evaluation.py
+++ evaluation.py
@@ -12,8 +12,33 @@ from rendertools import *
 from imageio import imwrite
 
 import json
+import tensorflow as tf
 
 import argparse
+
+def compute_sigma(pred_disp,points):
+    height, width = pred_disp.shape
+    
+    sigmas = []
+
+    for i in range(0, width, height):
+        square_pd = pred_disp[:, i:i+height]
+        square_ad = points[:, i:i+height]
+
+        pd = tf.math.log(square_pd)
+        p_depths = tf.where(~tf.math.is_finite(pd), tf.zeros_like(pd), pd)
+
+        ad = tf.math.log(tf.linalg.inv(square_ad))
+        a_depths = tf.where(~tf.math.is_finite(ad), tf.zeros_like(ad), ad)
+
+        s = tf.math.exp(tf.reduce_mean(p_depths + a_depths))
+        sigmas.append(s)
+
+    print(sigmas)
+    print(tf.reduce_mean(sigmas))
+    print()
+    return tf.reduce_mean(sigmas)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -34,7 +59,9 @@ if __name__ == '__main__':
 
     min_depth = 1.
     max_depth = 100.
-    depths = 1./np.linspace(1./max_depth, 1./min_depth, 32, endpoint=True)
+    depths = 1./np.linspace(1./max_depth, 1./min_depth,
+                            32, endpoint=True)
+    print(depths)
 
     regexr = '\/(\w+)\/(\w+).png'
     folder, image_name = re.findall(regexr, args.test_path)[0]
@@ -44,13 +71,22 @@ if __name__ == '__main__':
     with open(target_json_path) as f:
         target_json = json.load(f)
 
+    # Scale Depths 
+    predicted_depth = imread(os.path.join(args.cylinder_path, 'predicted_depth.png')).astype('float32')[:,:,0]
+    print(predicted_depth)
+    actual_depth = imread(os.path.join(args.cylinder_path, 'actual_depth.png')).astype('float32')
+    print(actual_depth)
+    #sigma = compute_sigma(predicted_depth, actual_depth) 
+    #print(sigma.numpy())
+    #depths = [i*sigma.numpy() for i in depths]
+    print(depths)
     meshes = [MyCylinder(bottom=-1*depth, top=1*depth, radius=depth,
                          texturepath=os.path.join(args.cylinder_path, 'layers/layer_%d.png' % i)) for i, depth in enumerate(depths)]
 
     renderer = Renderer(meshes, width=width, height=height,
                         offscreen=True)
 
-    eye = np.array([target_json["eye"]])
+    eye = np.array([target_json["eye"]]) * 0.5
     target = np.array(target_json["target"])
     up = np.array(target_json["up"])
 
@@ -59,7 +95,7 @@ if __name__ == '__main__':
     mvp_matrix = proj_matrix@view_matrix
 
     produced_image = renderer.render(mvp_matrix)
-    imwrite('produced_frame.png', produced_image)
+    imwrite('no_sigma_produced_frame.png', produced_image)
 
     target_image = imread(args.test_path)
 
